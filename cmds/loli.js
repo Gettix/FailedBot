5c11774603ddda459dd90301cbc33cc27f9711a9
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-12 14:03:50 +0700

    Create loli.js

---
+++ cmds/loli.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const agent = require("superagent");
+
+exports.run = async(client, message, args) => {
+    if(!message.channel.nsfw) return messge.reply("Использовать только в nsfw каналах.");
+
+    agent.get("https://nekos.life/api/v2/img/smallboobs").end((err, response) => {
+        const embed = new Discord.RichEmbed();
+        embed.setImage(response.body.url);
+        embed.setColor("RANDOM");
+        embed.setFooter("Powered by nekos.life", "https://i.imgur.com/hHlgnuW.png");
+        message.channel.send(embed);
+    });
+}
