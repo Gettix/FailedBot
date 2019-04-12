a639c2c2652a2539a806f94c01c70bc343fc20f5
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-12 12:26:55 +0700

    Update da.js

--- cmds/da.js
+++ cmds/da.js
@@ -1,10 +1,10 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
-    message.channel.send("Пизда!");
+    message.channel.send("Paka:3");
     let enbed = new Discord.RichEmbed() 
 };
 
 module.exports.help = {
-    name: "Da"
+    name: "Hello"
 };
