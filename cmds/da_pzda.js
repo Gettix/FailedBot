90bd41cc66003cfd163c6fb729fe3068d2208f62
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-12 13:51:40 +0700

    Create da_pzda.js

---
+++ cmds/da_pzda.js
@@ -0,0 +1,10 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    message.channel.send('п*зда');
+    let enbed = new Discord.RichEmbed() 
+};
+
+module.exports.help = {
+    name: "да"
+};
